9a6ceac3b7ae4a609d5a02fd9eeb67cbc4e59b81
Anne-Catherine Michaud <annecatherine.michaud@gmail.com>
2023-11-23 16:49:32 +0100

    Contact

--- screens/ContactsScreen.js
+++ screens/ContactsScreen.js
@@ -13,8 +13,27 @@ const ContactsScreen = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const { data } = await axios.get('http://localhost:8080/users')
-                setUsers(data)
+                const usersList = await axios.get('http://localhost:8080/users')
+                const contacts = await axios.get('http://localhost:8080/relations')
+                setUsers(usersList.data)
+
+                const a = contacts.data.filter((contact) => {
+                    return contact.id_user === userInfos.id_user
+                })
+
+                const b = usersList.data
+                    .map((user) => {
+                        const matchingContacts = a.filter((contact) => {
+                            return contact.id_contact === user.id_user
+                        })
+
+                        if (matchingContacts.length > 0) {
+                            return { ...user, matchingContacts }
+                        }
+                    })
+                    .filter(Boolean)
+
+                setUsers(b)
             } catch (error) {
                 console.error('Error fetching data:', error)
             }
@@ -24,12 +43,14 @@ const ContactsScreen = () => {
     }, [])
 
     const handleSearch = async (e) => {
-        const searchTerm = e.target.value
+        const searchTerm = e.target.value.toLowerCase()
         setSearchItem(searchTerm)
 
         if (searchTerm.trim() !== '') {
-            setUsers(users.filter((user) => user.email.includes(searchTerm)))
-            console.log(users)
+            const usersList = await axios.get('http://localhost:8080/users')
+            setUsers(
+                usersList.data.filter((user) => user.username.toLowerCase().includes(searchTerm))
+            )
         } else {
             const usersList = await axios.get('http://localhost:8080/users')
             const contacts = await axios.get('http://localhost:8080/relations')
@@ -50,18 +71,41 @@ const ContactsScreen = () => {
                     }
                 })
                 .filter(Boolean)
+            setUsers(b)
+        }
+    }
 
-            console.log(b)
+    const addContact = async (user) => {
+        try {
+            await axios.post(
+                'http://localhost:8080/relation',
+                {
+                    id_user: userInfos.id_user,
+                    id_contact: user,
+                    status: 'pending',
+                },
+                {
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                    },
+                }
+            )
+        } catch (error) {
+            console.error(error)
         }
     }
 
     return (
         <div>
             <input type="text" placeholder="Search" value={searchItem} onChange={handleSearch} />
-
             <ul>
                 {users.map((item) => (
-                    <li key={item.id_user}>{item.username}</li>
+                    <li key={item.id_user}>
+                        {item.username}{' '}
+                        {searchItem !== '' && (
+                            <button onClick={() => addContact(item.id_user)}>ajouter</button>
+                        )}
+                    </li>
                 ))}
             </ul>
         </div>
